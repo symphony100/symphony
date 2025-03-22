71c67ad3f8271b5d47580418a38e234496f4030a
symphony <okmte2011@gmail.com>
2025-03-22 11:34:41 +0900

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,13 @@
+// theme.js
+(function() {
+  // 保存されたテーマカラーを取得し、適用
+  const savedThemeColor = localStorage.getItem("themeColor");
+  const defaultColor = "#008DBD"; // デフォルトのテーマカラー
+  document.documentElement.style.setProperty("--header-bg-color", savedThemeColor || defaultColor);
+
+  // テーマカラーを保存する関数
+  window.setThemeColor = function(themeColor) {
+    localStorage.setItem("themeColor", themeColor);
+    document.documentElement.style.setProperty("--header-bg-color", themeColor);
+  };
+})();
