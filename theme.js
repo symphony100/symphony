7ecb966c70f0d15900cd69061724787fc4d41352
symphony <okmte2011@gmail.com>
2025-04-08 19:23:29 +0900

    Update theme.js

--- theme.js
+++ theme.js
@@ -4,10 +4,12 @@
   const savedThemeColor = localStorage.getItem("themeColor");
   const defaultColor = "#008DBD"; // デフォルトのテーマカラー
   document.documentElement.style.setProperty("--header-bg-color", savedThemeColor || defaultColor);
+  document.documentElement.style.setProperty("--news-title-color", themeColor); 
 
   // テーマカラーを保存する関数
   window.setThemeColor = function(themeColor) {
     localStorage.setItem("themeColor", themeColor);
     document.documentElement.style.setProperty("--header-bg-color", themeColor);
+    document.documentElement.style.setProperty("--news-title-color", themeColor); 
   };
 })();
