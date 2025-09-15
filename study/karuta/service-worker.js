fc6e6afd24e44bad7cfd662be1b477d0ee26f962
symphony <okmte2011@gmail.com>
2025-09-15 20:14:03 +0900

    Create service-worker.js

---
+++ study/karuta/service-worker.js
@@ -0,0 +1,24 @@
+const cacheName = "symphony-cache-v1";
+const assetsToCache = [
+  "/symphony/study/karuta/",
+  "/symphony/study/karuta/use/",
+  "/symphony/study/karuta/manifest.json",
+  "/symphony/study/karuta/tatami.png",
+  "/symphony/study/karuta/icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
