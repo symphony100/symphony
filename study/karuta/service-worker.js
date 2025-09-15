b752d580703d983589cd7748fbd0007373ebfcd2
symphony <okmte2011@gmail.com>
2025-09-15 20:37:45 +0900

    Update service-worker.js

--- study/karuta/service-worker.js
+++ study/karuta/service-worker.js
@@ -2,6 +2,7 @@ const cacheName = "symphony-cache-v1";
 const assetsToCache = [
   "/symphony/study/karuta/",
   "/symphony/study/karuta/use/",
+  "/symphony/study/karuta/cards.json",
   "/symphony/study/karuta/manifest.json",
   "/symphony/study/karuta/tatami.png",
   "/symphony/study/karuta/icon.png"
